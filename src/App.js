be0d697c9abc70cd0b8ab358d2cd2f2efdd377ce
sangwon11 <dbstkd0408@gmail.com>
2024-07-13 23:48:05 +0900

    day6

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ function App() {
     } else if (weather?.weather[0].main == 'Haze') {
       setBackgroundImage('url(https://plus.unsplash.com/premium_photo-1666211586138-d40960820390?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)')
     } else if (weather?.weather[0].main == 'Dust') {
-      setBackgroundImage('url()')
+      setBackgroundImage('url(https://images.unsplash.com/photo-1603695820889-f8a0a86b8712?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)')
     } else if (weather?.weather[0].main == 'Fog') {
       setBackgroundImage('url()')
     } else if (weather?.weather[0].main == 'sand') {
