07611a21cb117ae15dbfb891c5df3592f5ddbdef
sangwon11 <dbstkd0408@gmail.com>
2024-08-12 20:14:06 +0900

    1

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -6,12 +6,13 @@ const WeatherBox = ({weather, icon, setCity}) => {
   return (
       <div className='weather-box'>
         <SearchBar setCity={setCity}/>
-        <div>
+        <div className='weather-info'>
           <div>{icon}</div>
           <div>{weather?.name}</div>
           <h2>{weather?.main.temp}C / {Math.floor(weather?.main.temp * 1.8 +32)}F </h2>
           <h3>{weather?.weather[0].description}</h3>
         </div>
+        <img src='https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/19/5e/41/entrance.jpg?w=1000&h=-1&s=1' />
       </div>
   )
 }
